362b01dd21fe8b62214b1f8880ab77a1e4387306
Duong Dang <dddangmedia@gmail.com>
2019-11-04 08:05:33 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,34 @@
+class Book
+  def initialize(book_title)
+    @book_title = book_title
+  end
+  
+  def title 
+    @book_title
+  end
+  
+  def author=(author_name)
+    @author_name = author_name
+  end
+  
+  def author 
+    @author_name
+  end
+  
+  def page_count(num)
+    @page_count = num
+  end
+  
+  def page_count 
+    @page_count
+  end
+  
+  def genre=(genre)
+    @genre = genre
+  end
 
+  def genre
+    @genre
+  end
+end
 
